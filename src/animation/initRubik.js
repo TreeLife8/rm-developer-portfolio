9da6128d141e94aa7de3a57a743747a4a869bb18
TreeLife8 <85816400+TreeLife8@users.noreply.github.com>
2021-11-18 16:20:02 +1100

    Created the cube, rendering, lighting and animation function for the scene

--- src/animation/initRubik.js
+++ src/animation/initRubik.js
@@ -23,5 +23,48 @@ export function init() {
     1000
   );
   scene = new THREE.Scene();
-  camera.position.z = 0;
+  camera.position.z = 5;
+
+  // Rubik cube
+  geometry = new THREE.BoxGeometry(1.2, 1.2, 1.2);
+  const rubikTexture = new THREE.TextureLoader().load(
+    "/src/images/rubikCube.png"
+  );
+  material = new THREE.MeshPhongMaterial({
+    map: rubikTexture,
+  });
+  rubik = new THREE.Mesh(geometry, material);
+  scene.add(rubik);
+  rubik.position.setX(-3.5);
+  rubik.position.setZ(20);
+  rubik.position.setY(-0.3);
+
+  // Rendering
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.setAnimationLoop(animation);
+  renderer.setClearColor(0x272727, 1);
+  document.body.appendChild(renderer.domElement);
+
+  // Lighting
+  const pointLight = new THREE.PointLight(0xffffff);
+  pointLight.position.set(-4.5, 2, -1);
+  const ambientLight = new THREE.AmbientLight(0xffffff, 0.4);
+  scene.add(pointLight, ambientLight);
+
+  // Light & gid helpers
+  const lightHelper = new THREE.PointLightHelper(pointLight);
+  const gridHelper = new THREE.GridHelper(200, 5);
+  scene.add(lightHelper, gridHelper);
+
+  controls = new OrbitControls(camera, renderer.domElement);
+
+  // Animation
+  function animation(time) {
+    rubik.rotation.z = time / 4000;
+    rubik.rotation.x = time / 4000;
+    rubik.rotation.y = time / 2000;
+    controls.update();
+    renderer.render(scene, camera);
+  }
 }
